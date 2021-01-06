ff0bffed2b38f856b363779185d9f1380d2b979f
Gert <valigertt@gmail.com>
2021-01-06 23:39:13 +0300

    feat: add calendar and more icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,65 +1,40 @@
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import './navbar.scss';
 import {
-  Theme, makeStyles, fade, InputBase,
-} from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+  makeStyles, createStyles,
+} from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    width: 210,
-    marginLeft: 24,
-    float: 'right',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
+import TodayTwoToneIcon from '@material-ui/icons/TodayTwoTone';
+import SearchInput from '../search-input/Search-input';
+
+const useStyles = makeStyles(() => createStyles({
+  calendar: {
+    padding: 0,
   },
 }));
+
+const onClickSetting = () => {
+  console.log('settings on!');
+};
+
+const onClickCalendar = () => {
+  console.log('calendar on!');
+};
 
 const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
-      <InputBase
-        placeholder="Search…"
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput,
-        }}
-        inputProps={{ 'aria-label': 'search' }}
-      />
+    <div className="header">
+      <SearchInput />
+      <IconButton className={classes.calendar} onClick={onClickCalendar}>
+        <TodayTwoToneIcon className="header__calendar" />
+      </IconButton>
+      <IconButton onClick={onClickSetting}>
+        <MoreHorizIcon />
+      </IconButton>
     </div>
   );
 };
