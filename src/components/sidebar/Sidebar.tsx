4cbf9ff487fca1e528a1cefa85f384987ddb5665
Gert <valigertt@gmail.com>
2021-01-06 23:40:59 +0300

    refactor: refactor styles

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,19 +4,16 @@ import {
 } from '@material-ui/core/styles';
 import {
   List, ListItem, ListItemText, Button,
-  Drawer, ListItemIcon, Divider,
+  Drawer, ListItemIcon, Divider, fade,
 } from '@material-ui/core';
-import DateRangeTwoToneIcon from '@material-ui/icons/DateRangeTwoTone';
-import BubbleChartTwoToneIcon from '@material-ui/icons/BubbleChartTwoTone';
-import MenuTwoToneIcon from '@material-ui/icons/MenuTwoTone';
-import GradeTwoToneIcon from '@material-ui/icons/GradeTwoTone';
+import DateRange from '@material-ui/icons/DateRange';
+import BubbleChart from '@material-ui/icons/BubbleChart';
+import Menu from '@material-ui/icons/Menu';
+import Grade from '@material-ui/icons/Grade';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    display: 'flex',
-  },
   drawer: {
     [theme.breakpoints.up('sm')]: {
       width: drawerWidth,
@@ -29,24 +26,27 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
       marginLeft: drawerWidth,
     },
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+  link: {
+    color: '#5c7080',
+  },
+  linkItem: {
+    '&:hover': {
+      backgroundColor: fade('#fff', 0.15),
     },
+  },
+  divider: {
+    backgroundColor: fade('#fff', 0.15),
   },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
+    backgroundColor: '#182026',
   },
 }));
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
+
   const classes = useStyles();
 
   const toggleSidebar = (): void => {
@@ -61,22 +61,22 @@ const Sidebar = () => {
     <div>
       <Button onClick={toggleSidebar} variant="contained" color="primary">Hide</Button>
       <div className={classes.toolbar} />
-      <List>
-        <ListItem button onClick={onItemSelected}>
-          <ListItemIcon><DateRangeTwoToneIcon /></ListItemIcon>
+      <List className={classes.link}>
+        <ListItem button onClick={onItemSelected} className={classes.linkItem}>
+          <ListItemIcon><DateRange htmlColor="#5c7080" /></ListItemIcon>
           <ListItemText primary="DAILY NOTES" />
         </ListItem>
-        <ListItem button onClick={onItemSelected}>
-          <ListItemIcon><BubbleChartTwoToneIcon /></ListItemIcon>
+        <ListItem button onClick={onItemSelected} className={classes.linkItem}>
+          <ListItemIcon><BubbleChart htmlColor="#5c7080" /></ListItemIcon>
           <ListItemText primary="GRAPH OVERVIEW" />
         </ListItem>
-        <ListItem button onClick={onItemSelected}>
-          <ListItemIcon><MenuTwoToneIcon /></ListItemIcon>
+        <ListItem button onClick={onItemSelected} className={classes.linkItem}>
+          <ListItemIcon><Menu htmlColor="#5c7080" /></ListItemIcon>
           <ListItemText primary="ALL PAGES" />
         </ListItem>
-        <Divider />
-        <ListItem button onClick={onItemSelected}>
-          <ListItemIcon><GradeTwoToneIcon /></ListItemIcon>
+        <Divider className={classes.divider} />
+        <ListItem button onClick={onItemSelected} className={classes.linkItem}>
+          <ListItemIcon><Grade htmlColor="#5c7080" /></ListItemIcon>
           <ListItemText primary="SHORTCUTS" />
         </ListItem>
       </List>
@@ -88,21 +88,6 @@ const Sidebar = () => {
   }
   return (
     <nav className={classes.drawer} aria-label="mailbox folders">
-      <Button onClick={toggleSidebar} variant="contained" color="primary">Hide</Button>
-      {/* <Drawer */}
-      {/*  variant="temporary" */}
-      {/*  anchor="left" */}
-      {/*  open={mobileOpen} */}
-      {/*  onClose={handleDrawerToggle} */}
-      {/*  classes={{ */}
-      {/*    paper: classes.drawerPaper, */}
-      {/*  }} */}
-      {/*  ModalProps={{ */}
-      {/*    keepMounted: true, */}
-      {/*  }} */}
-      {/* > */}
-      {/*  {drawer} */}
-      {/* </Drawer> */}
       <Drawer
         classes={{
           paper: classes.drawerPaper,
