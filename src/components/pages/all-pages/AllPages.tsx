bcaa229b624c194178698b38440fddc7f8e5e7cc
Gert <valigertt@gmail.com>
2021-01-11 18:48:58 +0300

    init: init allPages styles

--- src/components/pages/all-pages/AllPages.tsx
+++ src/components/pages/all-pages/AllPages.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './allPages.scss';
 import { withRouter } from 'react-router-dom';
 
 const AllPages = () => (
-  <div>
+  <div className="pages-container">
     <h1>All pages</h1>
     <h2>All pages</h2>
   </div>
