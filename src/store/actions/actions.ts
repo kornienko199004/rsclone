2febca0e8e49d457291699e94eae92c686be6029
Gert <valigertt@gmail.com>
2021-01-19 17:07:22 +0300

    feat: add userLoggedIn action

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -1,4 +1,8 @@
 const ADD_NEIGHBOR = 'ADD_NEIGHBOR';
 const CHANGE_FOCUS_ELEMENT = 'CHANGE_FOCUS_ELEMENT';
 
-export { ADD_NEIGHBOR, CHANGE_FOCUS_ELEMENT };
+const userLoggedIn = () => ({
+  type: 'USER_LOGGED_IN',
+});
+
+export { ADD_NEIGHBOR, CHANGE_FOCUS_ELEMENT, userLoggedIn };
