3d65bf248a3cdd5662178bb1452a1db5f39c9b71
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2021-01-25 12:57:56 +0300

    Daily notes (#3)
    * feat: start to add special syntax for notes creation
    
    feat: start to add special syntax for notes creation
    
    * feat: process to implent special syntax
    
    * feat(daily-notes): start to add codemirror
    
    * feat(daily-notes): implement daily-notes
    
    * refactor(daily-notes): remove console.log
    
    * refactor(daily-notes): lint commit
    
    * refactor: remove unused packages
    
    Co-authored-by: Корниенко Алексей Александрович <Kornienko-A@gis.lan>

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './components/app/App';
 import reducer from './store/reducers/reducer';
+import { IPage } from './models/notes.model';
 
 // const rsCloneService = new RSCloneService();
 
 const initialState : {
     isLoggedIn: boolean,
     notes: [],
+    noteBody: IPage[],
     focusComponentPath: (string | number)[]
     userData: {username: string, email: string}
 } = {
@@ -20,10 +23,12 @@ const initialState : {
     email: '',
   },
   notes: [],
+  noteBody: [],
   focusComponentPath: [0],
 };
 
-const store = createStore(reducer, initialState);
+const composedEnhancer = applyMiddleware(thunkMiddleware);
+const store = createStore(reducer, initialState, composedEnhancer);
 
 ReactDOM.render(
   <Provider store={store}>
