d3418cdf1dc89ed80083afdcb2952ef070010276
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-01-28 18:21:35 +0300

    feat: change graph package

--- src/components/pages/application-page/graph-overview/GraphOverview.tsx
+++ src/components/pages/application-page/graph-overview/GraphOverview.tsx
@@ -1,19 +1,19 @@
+/* eslint-disable no-console */
 /* eslint-disable camelcase */
 /* eslint-disable no-underscore-dangle */
 import React, { useContext, useEffect, useState } from 'react';
 import './graphOverview.scss';
-import NoteGraph from 'react-graph-network';
-import { withRouter } from 'react-router-dom';
+import Graph from 'react-graph-vis';
+import { Redirect, withRouter } from 'react-router-dom';
 import RSCloneServiceContext from '../../../rsCloneServiceContext/index';
-import Node from './Node';
 
-export interface ModificationNote {
+interface ModificationNote {
   _id: string;
   modified_on: string;
   modified_by: null | string;
   modification_note: null | string;
 }
-export interface Note {
+interface Note {
   _id: string;
   title: string;
   body: object;
@@ -23,31 +23,33 @@ export interface Note {
   __v: number;
 }
 
-export const getNodes = async (res: Note[]) => {
+const getNodes = async (res: Note[]) => {
   const nodes: {
     id: string;
-    title: string;
+    label: string;
   }[] = await res.map((note: Note) => ({
-    id: note._id,
-    title: note.title,
+    id: note.title,
+    label: note.title,
   }));
-  const links: any = [];
-  res.map((note: Note) => {
-    const thisId = note._id;
-    return note.parents.forEach((parent: any) => links.push({
-      source: thisId,
-      target: parent.id,
-
-    }));
-  });
+  const edges: {
+    from: string;
+    to: string;
+  }[] = [];
+  res.map((note: Note) => note.parents.forEach(async (parent: any) => {
+    edges.push({
+      from: note.title,
+      to: parent.pageLink,
+    });
+  }));
   return ({
     nodes,
-    links,
+    edges,
   });
 };
 
-const Graph = () => {
-  const [notes, setNotes]: any = useState(null);
+const myGraph = () => {
+  const [notes, setNotes] = useState(null as any);
+  const [link, setLink] = useState('');
   const service = useContext(RSCloneServiceContext);
 
   useEffect(() => {
@@ -59,22 +61,54 @@ const Graph = () => {
     getInfo();
   }, []);
 
+  const options = {
+    autoResize: true,
+    // height: '100%',
+    height: '500px',
+    width: '100%',
+    locale: 'en',
+    nodes: {
+      color: '#3f51b5',
+      fixed: false,
+      font: '18px BlinkMacSystemFont #000000',
+      shape: 'dot',
+    },
+    edges: {
+      arrows: {
+        to: {
+          enabled: false,
+          type: 'bar',
+        },
+      },
+      color: 'black',
+      scaling: {
+        label: true,
+      },
+    },
+    layout: {
+      hierarchical: true,
+    },
+  };
+
   return (
-    <div className="graph-overview" style={{ height: '80vh' }}>
-      {notes && (
-        <NoteGraph
-          NodeComponent={Node}
-          nodeDistance={800}
-          distance={50}
-          zoomDepth={1}
-          hoverOpacity={0.3}
-          enableDrag
-          pullIn={false}
-          data={notes}
-          id="graph"
-        />
-      )}
-    </div>
+    (link)
+      ? <Redirect to={link} />
+      : (
+        notes && (
+          <Graph
+            graph={notes}
+            options={options}
+            events={{
+              click: (event: { nodes: any; edges: any; }) => {
+                const { nodes } = event;
+                if (nodes[0]) {
+                  setLink(`/app/note/${nodes[0]}`);
+                }
+              },
+            }}
+          />
+        )
+      )
   );
 };
-export default withRouter(Graph);
+export default withRouter(myGraph);
